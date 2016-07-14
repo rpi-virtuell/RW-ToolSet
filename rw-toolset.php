c3eec4610d5fe1c5020a6415cbf46bd82e31b4a3
Joachim Happel <joachim.happel@gmail.com>
2016-07-14 20:49:50 +0200

    Fixes Loading BP-Docs-Foldercontent with Groups-Hierarchy

--- rw-toolset.php
+++ rw-toolset.php
@@ -1,9 +1,33 @@
 <?php
 /*
-Plugin Name: RW Toolset
-Description: Erlaubt zusätzliche Dateitypen. Setze übergeordnete Sprachdateien.
+ * Plugin Name: RW Toolset
+ * Plugin Uri:  https://github.com/rpi-virtuell/RW-ToolSet
+ * Description: rpi-virtuell Hacks für Buddypress Instanzen. Erlaubt zusätzliche Dateitypen. Setze übergeordnete Sprachdateien. Fixed verschiedene Plugin Probleme
+ * Author:      Joachim Happel
+ * Author URI:  https://bbpress.org
+ * Version:     0.0.1
+ /
+
+/**
+* Fixes Loading BP-Docs-Foldercontent via Ajax
+* issue: in Ajax  Buddypress-Grpoups-Component was not overriden by the Groupshierarchy component 
+*        the result was: folder contents left empty in child groups
+* this little hack correct the missing group_id
 */
 
+add_filter('bp_get_current_group_id','bp_get_current_group_id_in_group_hierarchy',999,2);
+function bp_get_current_group_id_in_group_hierarchy($current_group_id, $current_group){
+		
+	if( $_SERVER['HTTP_X_REQUESTED_WITH'] && isset($_GET['group_id']) && isset($_GET['action']) && $_GET['action'] == 'bp_docs_get_folder_content' ){
+		return $_GET['group_id'];
+	}
+	
+	return $current_group_id;
+}
+
+/**
+* Description: Erlaubt den Upload zusätzlicher Dateitypen. Setze übergeordnete Sprachdateien.
+*/
 
 add_action('init', 'reliwerk_interim_solution',20);
 
@@ -20,6 +44,10 @@ function add_custom_mime_types($mimes){
 	));
 }
 
+/**
+ * Setze übergeordnete Sprachdateien für plugins
+ * Sprachdateien in wp-content/languages/plugins werden bevorzugt
+ */
 function rw_fix_correct_plugin_textdomain_dir() {
 	
 	$locale = get_locale();
@@ -39,6 +67,11 @@ function rw_fix_correct_plugin_textdomain_dir() {
 	}
 }
 add_action( 'plugins_loaded', 'rw_fix_correct_plugin_textdomain_dir' );
+
+/**
+ * Setze übergeordnete Sprachdateien für themes
+ * Sprachdateien in wp-content/languages/themes werden bevorzugt
+ */
 
 function rw_fix_correct_theme_textdomain_dir() {
 	
